d5f7cc47be10a15fab29ec3751708ec455fa71cb
Nikolay Selicky <109921087+N1i2@users.noreply.github.com>
2025-06-25 14:57:43 +0300

    deleted unnecessary files

--- RoomRentalSystemBackend/RoomRentalSystem.Domain/Constants/RegexPatterns.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RoomRentalSystem.Domain.Constants
-{
-    public static class RegexPatterns
-    {
-        public const string Email = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
-        public const string PhoneNumber = @"^\+375\s?(17|25|29|33|44)\d{3}-?\d{2}-?\d{2}$";
-    }
-}
--- RoomRentalSystemBackend/RoomRentalSystem.Domain/Constants/RoleNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RoomRentalSystem.Domain.Constants
-{
-    public static class RoleNames
-    {
-        public const string Guest = "Guest";
-        public const string Owner = "Owner";
-        public const string Admin = "Admin";
-    }
-}
